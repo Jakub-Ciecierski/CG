c2a496ec2a2d62c48ccc8124b0d80debecd40e56
j.ciecierski <jakub.ciecier@gmail.com>
2015-04-26 14:19:03 +0200

    Drawer project, finished

---
+++ Drawing/Drawing/Drawer.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing
+{
+    public class Drawer
+    {
+        private int thickness;
+
+        public int Thickness
+        {
+            get { return thickness; }
+            set { thickness = value; }
+        }
+
+        public Drawer(int thickness)
+        {
+            Thickness = thickness;
+        }
+
+        public void DrawSymmetricLine(int x1, int y1, int x2, int y2, Action<int, int> putPixel)
+        {
+            // if the line too steep, "kick" the plane around
+            bool steep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+            if (steep)
+            {
+                int tmpX1 = x1;
+                x1 = y1;
+                y1 = tmpX1;
+
+                int tmpX2 = x2;
+                x2 = y2;
+                y2 = tmpX2;
+            }
+            // if x1 is greater than x2, the starting point
+            if (x1 > x2)
+            {
+                int tmpX1 = x1;
+                x1 = x2;
+                x2 = tmpX1;
+
+                int tmpY1 = y1;
+                y1 = y2;
+                y2 = tmpY1;
+            }
+
+            int dx = x2 - x1;
+            int dy = Math.Abs(y2 - y1);
+
+            int d = 2 * dy - dx;
+            int dE = 2 * dy;
+            int dNE = 2 * (dy - dx);
+
+            // front and back
+            int xf = x1, yf = y1;
+            int xb = x2, yb = y2;
+
+            int ystep = (y1 < y2) ? 1 : -1;
+
+            if(steep)
+            {
+                putThickPixel(yf, xf, putPixel);
+                putThickPixel(yb, xb, putPixel);
+            }
+            else
+            {
+                putThickPixel(xf, yf, putPixel);
+                putThickPixel(xb, yb, putPixel);
+            }
+
+            while (xf < xb)
+            {
+                ++xf;
+                --xb;
+                if (d < 0)
+                    d += dE;
+                else
+                {
+                    d += dNE;
+                    yf += ystep;
+                    yb -= ystep;
+                }
+                if (steep)
+                {
+                    putThickPixel(yf, xf, putPixel);
+                    putThickPixel(yb, xb, putPixel);
+                }
+                else
+                {
+                    putThickPixel(xf, yf, putPixel);
+                    putThickPixel(xb, yb, putPixel);
+                }
+            }
+        }
+
+        public void DrawCircle(int xc, int yc, int R, Action<int, int> putPixel)
+        {
+            int dE = 3;
+            int dSE = 5 - 2 * R;
+            int d = 1 - R;
+
+            int x = 0;
+            int y = R;
+
+            while (y > x)
+            {
+                if (d < 0)
+                {
+                    d += dE;
+                    dE += 2;
+                    dSE += 2;
+                }
+                else
+                {
+                    d += dSE;
+                    dE += 2;
+                    dSE += 4;
+                    --y;
+                }
+                ++x;
+                drawAllOctans(xc, yc, x, y, putPixel);
+            }
+        }
+
+        private void drawAllOctans(int xc, int yc, int x, int y, Action<int, int> putPixel)
+        {
+            putThickPixel(xc + x, yc + y, putPixel);
+            putThickPixel(xc - x, yc + y, putPixel);
+            putThickPixel(xc + x, yc - y, putPixel);
+            putThickPixel(xc - x, yc - y, putPixel);
+            putThickPixel(xc + y, yc + x, putPixel);
+            putThickPixel(xc - y, yc + x, putPixel);
+            putThickPixel(xc + y, yc - x, putPixel);
+            putThickPixel(xc - y, yc - x, putPixel);
+        }
+
+        private void putThickPixel(int x, int y, Action<int, int> putPixel)
+        {
+            if (Thickness == 1)
+                thickness1(x, y, putPixel);
+            else if (Thickness == 3)
+                thickness3(x, y, putPixel);
+            else if (Thickness == 5)
+                thickness5(x, y, putPixel);
+            else if (Thickness == 7)
+                thickness7(x, y, putPixel);
+            else
+                thickness1(x, y, putPixel);
+        }
+
+        private void thickness1(int x, int y, Action<int, int> putPixel)
+        {
+            putPixel(x, y);
+        }
+
+        private void thickness3(int x, int y, Action<int, int> putPixel)
+        {
+            putPixel(x, y + 1);
+            putPixel(x + 1, y);
+            putPixel(x, y);
+            putPixel(x, y - 1);
+            putPixel(x - 1, y);
+        }
+
+        private void thickness5(int x, int y, Action<int, int> putPixel)
+        {
+            putPixel(x - 1, y + 2);
+            putPixel(x, y + 2);
+            putPixel(x+1, y + 2);
+
+            putPixel(x - 2, y + 1);
+            putPixel(x - 1, y + 1);
+            putPixel(x, y + 1);
+            putPixel(x + 1, y + 1);
+            putPixel(x + 2, y + 1);
+
+            putPixel(x - 2, y);
+            putPixel(x - 1, y);
+            putPixel(x, y);
+            putPixel(x + 1, y);
+            putPixel(x + 2, y);
+
+            putPixel(x - 2, y - 1);
+            putPixel(x - 1, y - 1);
+            putPixel(x, y - 1);
+            putPixel(x + 1, y - 1);
+            putPixel(x + 2, y - 1);
+
+            putPixel(x - 1, y - 2);
+            putPixel(x, y - 2);
+            putPixel(x + 1, y - 2);
+        }
+
+        private void thickness7(int x, int y, Action<int, int> putPixel)
+        {
+            putPixel(x - 1, y + 3);
+            putPixel(x,     y + 3);
+            putPixel(x + 1, y + 3);
+
+            putPixel(x - 2, y + 2);
+            putPixel(x - 1, y + 2);
+            putPixel(x,     y + 2);
+            putPixel(x + 1, y + 2);
+            putPixel(x + 2, y + 2);
+
+            putPixel(x - 3, y + 1);
+            putPixel(x - 2, y + 1);
+            putPixel(x - 1, y + 1);
+            putPixel(x,     y + 1);
+            putPixel(x + 1, y + 1);
+            putPixel(x + 2, y + 1);
+            putPixel(x + 3, y + 1);
+
+            putPixel(x - 3, y);
+            putPixel(x - 2, y);
+            putPixel(x - 1, y);
+            putPixel(x,     y);
+            putPixel(x + 1, y);
+            putPixel(x + 2, y);
+            putPixel(x + 3, y);
+
+            putPixel(x - 3, y - 1);
+            putPixel(x - 2, y - 1);
+            putPixel(x - 1, y - 1);
+            putPixel(x,     y - 1);
+            putPixel(x + 1, y - 1);
+            putPixel(x + 2, y - 1);
+            putPixel(x + 3, y - 1);
+
+            putPixel(x - 2, y - 2);
+            putPixel(x - 1, y - 2);
+            putPixel(x, y - 2);
+            putPixel(x + 1, y - 2);
+            putPixel(x + 2, y - 2);
+
+            putPixel(x - 1, y - 3);
+            putPixel(x, y - 3);
+            putPixel(x + 1, y - 3);
+        }
+    }
+}
---
+++ Drawing/DrawingGUI/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DrawingGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SolidColorBrush brush = new SolidColorBrush() { Color = Colors.Black };
+
+        Point point1;
+        Point point2;
+
+        Drawer drawer;
+
+        bool whichToDraw = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            drawer = new Drawer(1);
+        }
+
+        private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            point1 = e.GetPosition(canvas);
+        }
+
+        private void canvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            point2 = e.GetPosition(canvas);
+
+            if (point1 != null)
+            {
+                new Thread(() =>
+                {
+                    Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() =>
+                    {
+                        if (whichToDraw)
+                            drawLine(point1, point2);
+                        else
+                            drawCircle(point1, point2);
+                    }));
+                    
+                }).Start();
+            }
+        }
+
+        private void drawCircle(Point point1, Point point2)
+        {
+            int x2 = (int)point2.X;
+            int x1 = (int)point1.X;
+            int y2 = (int)point2.Y;
+            int y1 = (int)point1.Y;
+
+            int R = (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            drawer.DrawCircle((int)point1.X, (int)canvas.ActualHeight - (int)point1.Y, R, putPixel);
+        }
+
+        private void drawLine(Point point1, Point point2)
+        {
+            drawer.DrawSymmetricLine((int)point1.X, (int)canvas.ActualHeight - (int)point1.Y, 
+                            (int)point2.X, (int)canvas.ActualHeight - (int)point2.Y , putPixel);
+        }
+
+        private void putPixel(int x, int y)
+        {
+
+            System.Windows.Shapes.Rectangle rect;
+            rect = new System.Windows.Shapes.Rectangle();
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.Fill = new SolidColorBrush(Colors.Black);
+            rect.StrokeThickness = 1;
+            rect.Width = 1;
+            rect.Height = 1;
+
+            canvas.Children.Add(rect);
+            Canvas.SetLeft(rect, x);
+            Canvas.SetTop(rect, (int)canvas.ActualHeight - y);
+        }
+
+        private void lineButton_Click(object sender, RoutedEventArgs e)
+        {
+            whichToDraw = true;
+        }
+
+        private void circleButton_Click(object sender, RoutedEventArgs e)
+        {
+            whichToDraw = false;
+        }
+
+        private void ComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            ComboBox box = sender as ComboBox;
+            if (box.Text != "")
+                drawer.Thickness = System.Convert.ToInt32(box.Text);
+        }
+    }
+}
